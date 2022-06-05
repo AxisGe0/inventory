5934919d70eb7fee3c84dca4fa6ede0c36ffa581
AxisGe0 <69110100+AxisGe0@users.noreply.github.com>
2022-06-05 21:51:45 +0530

    Add files via upload

--- main/shared.lua
+++ main/shared.lua
@@ -1,12 +1,14 @@
 Shared = {
     PlayerWeight = 120000,
     Slots = 66,
+    Debug = true,
     Items = {
         ['lockpick'] = {
             label = 'Lockpick',
             weight = 1000,
             unique = false,
             Close = false,
+            Decaytime = 60,
             Use = function(item)
                 print('Lockpick Used',json.encode(item))
             end,
@@ -16,9 +18,22 @@ Shared = {
             weight = 1000,
             unique = false,
             Close = false,
+            Decaytime = 60,
             Use = function(item)
                 print('Water Used',json.encode(item))
             end,
         }
     },
 }
+if not Shared.Debug then 
+    print = function() end
+end
+
+GetQualityPercentage = function(item,time)
+    local current = os.time() 
+    if not Shared.Items[item].Decaytime then 
+        return 100 
+    end
+    local percentage = 100 - math.ceil((((current - time) / Shared.Items[item].Decaytime) * 100))
+    return percentage > 0 and percentage or 0
+end
